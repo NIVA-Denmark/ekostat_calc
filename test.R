b9bcd430e5dd29509fc157e32cfef32b105c14d7
ciaran-dk <ciaran@murraynet.dk>
2018-06-06 15:48:59 +0200

    updated varcomp for Secchi

--- test.R
+++ test.R
@@ -15,37 +15,70 @@ source("ReadBounds.R")
 source("ReadIndicatorType.R")
 source("ReadVariances.R")
 source("Assessment.R")
+source("ReadBathymetry.R")
 
 load("data/SASdata.Rda")
 dfall<-df
 
 df.bounds<-ReadBounds()
+df.bounds.hypox<-ReadBoundsHypoxicArea()
 df.indicators<-ReadIndicatorType()
 df.variances<-ReadVariances()
+df.bathy<-ReadBathymetry()
+
+
+
+# WBselect<-"SE570000-120701"
+# type<-"4"
+# indname<-"CoastOxygen"
+# varname<-"O2"
+# MonthInclude <- c(1,2,3,4,5,6,7,8,9,10,11,12)
+# yr_from<-2010
+# yr_to<-2015
 
 WBselect<-"SE572135-120141"
 type<-"1s"
-varname<-"CoastSecchiEQR"
-
-varlist<- VarianceComponents(df.indicators,df.variances,type,varname)
-RefCond_sali<-SalinityReferenceValues(df.bounds,"1s",varname,missing=1)
-
+indname<-"CoastSecchiEQR"
+varname<-"secchi"
 MonthInclude <- c(6,7,8)
+yr_from<-2005
+yr_to<-2009
 
+varlist<- VarianceComponents(df.indicators,df.variances,type,indname)
+RefCond_sali<-SalinityReferenceValues(df.bounds,"1s",indname,missing=1)
+
+
+BoundariesHypoxicArea <<- df.bounds.hypox %>% filter(WB==WBselect) %>% select(Worst,P.B,M.P,G.M,H.G,RefCond) %>% as.list()
+WB_bathymetry <<- df.bathy %>% filter(WB==WBselect) %>% select(area_pct,depth)
+# ---------------------- if no bathymetry available, use alternative --------------
+# if(!nrow(WB_bathymetry)>0){
+#   area_pct<-seq(1,100,by=1) 
+#   depth<-area_pct
+#   WB_bathymetry<<-data.frame(area_pct,depth)
+# }
+if(!nrow(df.bounds.hypox %>% filter(WB==WBselect))>0){
+  BoundariesHypoxicArea <<- df.bounds.hypox %>% filter(WB=="SE582000-115270") %>% select(Worst,P.B,M.P,G.M,H.G,RefCond) %>% as.list()
+}
+# -----------------------------------------
 df <- dfall %>% filter(WB_ID==WBselect) %>% 
-  mutate(xvar=secchi) %>% filter(!is.na(xvar))
-res<-CalculateIndicator(varname,df,RefCond_sali,varlist,MonthInclude,2007,2012,n_iter=200)
+  mutate_("xvar"=varname) %>% filter(!is.na(xvar))
+res<-CalculateIndicator(indname,df,RefCond_sali,varlist,MonthInclude,yr_from,yr_to,n_iter=1000)
 
 Value<-res$indicator_sim
-dfres<-as.data.frame(Value) %>% mutate(Type=type,Indicator=varname)
+dfres<-as.data.frame(Value) %>% mutate(Type=type,Indicator=indname)
 
-bounds <- df.bounds %>% filter(Indicator==varname,Type==type) %>%
+bounds <- df.bounds %>% filter(Indicator==indname,Type==type) %>%
   select(Indicator,Type,RefCond,H.G,G.M,M.P,P.B,Worst)
 
 dfres<-dfres %>% left_join(bounds,by=c("Type","Indicator"))
 dfres<-GetClass(dfres)
 
-ggplot(dfres,aes(Value)) + geom_histogram()
-ggplot(dfres,aes(ClassID)) + geom_histogram()
+list<-c(bounds[1,4],bounds[1,5],bounds[1,6],bounds[1,7])
+clist<-c('#00B0F0','#92D050','#FFFF00', '#FFC000')
+#clist<-c('#FF0000','#FFC000','#FFFF00','#92D050','#00B0F0')
+
+ggplot(dfres,aes(Value)) + geom_histogram(binwidth=0.02) + theme_classic() +
+  geom_vline(xintercept=list,colour=clist)
+ggplot(dfres,aes(ClassID)) + geom_bar() + theme_classic()
 
 #dfresx<-dfres
--- test_O2.R
+++ test_O2.R
@@ -23,29 +23,39 @@ df <- ReadMonitoringDataSMHI("data/ByfjordenO2x_2007_2012.sas7bdat")
 WB_bathymetry <- data.frame(area_pct = 1:100, depth = c(1:40/4,10+1:20/2,20+1:30/3,30+1:10))
 BoundariesHypoxicArea <- c(100,68,64,60,40,0)
 df <- mutate(df,xvar=O2)
-#CalculateIndicator("CoastOxygen",filter(df,year>2009),RefCond_sali,variance_list,MonthInclude,2007,2012,n_iter=100)
+res<-CalculateIndicator("CoastOxygen",filter(df,year>2009),RefCond_sali,variance_list,MonthInclude,2007,2012,n_iter=1000)
+
+Value<-res$indicator_sim
+dfres<-as.data.frame(Value)
+ggplot(dfres,aes(Value)) + geom_histogram(binwidth=0.01)
+
+
 df <- ReadMonitoringDataSMHI("data/GullmarnO2x_2007_2012.sas7bdat")
 WB_bathymetry <- data.frame(area_pct = 1:100, depth = c(1:40*2,80+1:20,100+1:30,130+1:10))
 BoundariesHypoxicArea <- c(100,82,53,24,16,0)
 df <- mutate(df,xvar=O2)
-#CalculateIndicator("CoastOxygen",df,RefCond_sali,variance_list,MonthInclude,2007,2012,n_iter=100)
+res<-CalculateIndicator("CoastOxygen",df,RefCond_sali,variance_list,MonthInclude,2007,2012,n_iter=1000)
 
-# Test using other stations
+Value<-res$indicator_sim
+dfres<-as.data.frame(Value)
+ggplot(dfres,aes(Value)) + geom_histogram(binwidth=0.01)
 
-load("data/SASdata.Rda")
-dfall<-df
-
-WBselect<-"SE563000-123351"
-df <- dfall %>% filter(WB_ID==WBselect) %>% 
-  mutate(xvar=O2) %>% filter(!is.na(xvar))
-#CalculateIndicator("CoastOxygen",df,RefCond_sali,variance_list,MonthInclude,2004,2009,n_iter=100)
-#CalculateIndicator("CoastOxygen",df,RefCond_sali,variance_list,MonthInclude,2010,2015,n_iter=100)
-
-
-df.bounds<-ReadBounds()
-WBselect<-"SE572135-120141"
-df <- dfall %>% filter(WB_ID==WBselect) %>% 
-  mutate(xvar=secchi) %>% filter(!is.na(xvar))
-CalculateIndicator("CoastSecchiEQR",df,RefCond_sali,variance_list,MonthInclude,2007,2012,n_iter=100)
-
+# # Test using other stations
+# 
+# load("data/SASdata.Rda")
+# dfall<-df
+# 
+# WBselect<-"SE563000-123351"
+# df <- dfall %>% filter(WB_ID==WBselect) %>% 
+#   mutate(xvar=O2) %>% filter(!is.na(xvar))
+# #CalculateIndicator("CoastOxygen",df,RefCond_sali,variance_list,MonthInclude,2004,2009,n_iter=100)
+# #CalculateIndicator("CoastOxygen",df,RefCond_sali,variance_list,MonthInclude,2010,2015,n_iter=100)
+# 
+# 
+# df.bounds<-ReadBounds()
+# WBselect<-"SE572135-120141"
+# df <- dfall %>% filter(WB_ID==WBselect) %>% 
+#   mutate(xvar=secchi) %>% filter(!is.na(xvar))
+# CalculateIndicator("CoastSecchiEQR",df,RefCond_sali,variance_list,MonthInclude,2007,2012,n_iter=100)
+# 
 
