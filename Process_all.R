7bf6c6d1b1b6ec3c16dbd5983bf617d125618516
ciaran-dk <ciaran@murraynet.dk>
2018-11-20 18:42:36 +0100

    fixed err reading boundaries.txt

--- Process_all.R
+++ Process_all.R
@@ -154,11 +154,6 @@ bAPP<-FALSE
 #bOVR<-FALSE
 #bAPP<-TRUE
 
-mtcars %>%
-  split(.$cyl) %>% # from base R
-  map(~ lm(mpg ~ wt, data = .)) %>%
-  map(summary) %>%
-  map_dbl("r.squared")
 
 dflist <- df %>% split(.$WB)
 
--- ReadBounds.R
+++ ReadBounds.R
@@ -1,6 +1,14 @@
 ReadBounds<-function(){
   df<-read.table("data/boundaries.txt", fileEncoding = "UTF-8", sep="\t", 
                  stringsAsFactors=F, header=T, comment.char="",na.string = "#N/A")
+  
+  df$RefCond<-as.numeric(df$RefCond)
+  df$H.G<-as.numeric(df$H.G)
+  df$G.M<-as.numeric(df$G.M)
+  df$M.P<-as.numeric(df$M.P)
+  df$P.B<-as.numeric(df$P.B)
+  df$Sali_0<-as.numeric(df$Sali_0)
+  
   return(df)
 }
 
